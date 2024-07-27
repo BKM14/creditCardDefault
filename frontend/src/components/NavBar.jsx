8415663bd85d09b55ee2997605f1a7f7e0a271ce
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2024-07-27 17:12:41 +0530

    requirements.txt

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -8,9 +8,9 @@ const NavBar = () => {
             <Link to={'/'} className='ml-2 font-bold text-lg md:text-xl lg:text-3xl'>CreditGuard</Link >
         </div> 
         <div className='flex items-center text-sm md:text-lg lg:text-xl'>
-            <Link to={'/visualizations'} className='mx-1  md:mx-4 hover:bg-red-600 hover:font-bold hover:text-white duration-300 p-2 rounded'>Visualizations</Link >
-            <Link to={'/features'} className='mx-1  md:mx-4 hover:bg-red-600 hover:font-bold hover:text-white duration-300 p-2 rounded'>Features</Link >
-            <Link to={'/contact'} className='mx-1  md:mx-4 hover:bg-red-600 hover:font-bold hover:text-white duration-300 p-2 rounded'>Contact</Link >
+            <Link to={'/visualizations'} className='mx-1 md:mx-4 hover:bg-red-600 hover:font-bold hover:text-white duration-300 p-2 rounded'>Visualizations</Link >
+            <Link to={'/features'} className='mx-1 md:mx-4 hover:bg-red-600 hover:font-bold hover:text-white duration-300 p-2 rounded'>Features</Link >
+            <Link to={'/contact'} className='mx-1 md:mx-4 hover:bg-red-600 hover:font-bold hover:text-white duration-300 p-2 rounded'>Contact</Link >
         </div>
     </header>
   )
