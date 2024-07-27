83e6ee5cf174d4f197ab3d73c29a39d9a05e8d04
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2024-07-27 12:40:51 +0530

    update links and requirements

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -15,7 +15,7 @@ const Form = () => {
 
     const onSubmit = async () => {
         const requestObject = createRequestObject(getValues(), selectedModels);
-        const response = await fetch("http://127.0.0.1:5000/predict", {
+        const response = await fetch("https://creditcarddefault.onrender.com/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
